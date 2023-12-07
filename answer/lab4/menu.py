2f14b0b9b69366e0d64d9e031b1da826877bfb9e
wahle <3052748978@qq.com>
2023-12-07 12:00:16 +0800

    add module file

--- answer/lab4/menu.py
+++ answer/lab4/menu.py
@@ -1,46 +1,7 @@
-'''
-python注意事项:
-1.在input函数调用时,所有输入均转化成字符串
-2.对数据库的更新、删除、插入都要通过cnx.commit提交到数据库
-主函数文件
-
-'''
-import pymysql
-import mysql.connector
-import os
-import getpass
 import studentOP
-import studentOP
-import menu
 
-
-def clear_cmd():
-    os.system("clear")
-
-def pause_cmd():
-    os.system("pause")
-
-# 连接数据库
-def login():
-    clear_cmd()
-    username = input("请输入用户名\n")
-    passwd = getpass.getpass("请输入密码\n")
-
-    try:
-        cnx = mysql.connector.connect(
-            user = username,
-            host = 'localhost',   
-            password =  passwd,
-            database = 'CSEDB_U202212097'
-        )
-    except mysql.connector.errors:
-        print("连接失败\n")
-        return 0
-    print("连接成功")
-    return cnx
-'''''
 # 学生信息表操作
-def menu_student_modified():
+def menu_student_modified(cnx):
     choice = 1
     while choice != 0:
         print("\t\t\t学生信息表维护菜单\n\t1.查询所有学生信息\t2.插入学生信息\t3.更新学生信息\n\t4.删除学生记录\t0.退出学生表操作\n 请输入你要执行的操作")
@@ -89,12 +50,3 @@ def menu_student_modified():
 
         if op == '0':
             choice = 0
-'''''
-def main():
-    global cnx
-    cnx = login()
-    menu.menu_student_modified(cnx)
-    print("已退出")
-    cnx.close()
-
-main()
---
+++ answer/lab4/studentOP.py
@@ -0,0 +1,66 @@
+import os
+
+#学生表操作集合
+#查找所有学生信息
+
+
+def query_all_studentInfo(cnx):
+    cursor = cnx.cursor()
+    query = 'select * from student'
+    ret = '%s\t\t%s\t%s\t%s\t%s\t%s\t\n' % ('sno', 'sname', 'ssex', 'sage', 'sdept', 'scholarship')
+    cursor.execute(query)
+    for (sno, sname, ssex, sage, sdept, scholarship) in cursor:
+        ret += '%s\t%s\t%s\t%d\t%s\t%s\t\n' % (sno, sname, ssex, sage, sdept, scholarship)
+    return ret
+
+# 插入学生信息
+def insert_studentInfo(cnx, ssno, ssname, sssex, ssage, ssdept,sscholarship):
+    cursor = cnx.cursor()
+    
+    query = "insert into student values('%s', '%s', '%s', %d, '%s', '%s')" \
+                % (ssno, ssname, sssex, ssage, ssdept,sscholarship)
+    try:
+        cursor.execute(query)
+    except mysql.connector.errors:
+        return False
+    cnx.commit()
+    return True
+
+# 删除学生记录
+def delete_studentInfo(cnx, condition):
+    cursor = cnx.cursor()
+    query = "delete from student where sno='%s'" % (condition)
+    try:
+        cursor.execute(query)
+    except mysql.connector.errors:
+        return False
+    cnx.commit()
+    return True
+
+# 更新学生信息
+def update_studentInfo(cnx, Sno, column, new):
+    cursor = cnx.cursor()
+    if column == '1':
+        column = 'sname'
+    elif column == '2':
+        column = 'ssex'
+    elif column == '3':
+        column = 'sage'
+    elif column == '4':
+        column = 'sdept'
+    elif column == '5':
+        column = 'sscholarship'
+    
+    #拼接更新操作
+    if column != 'sage':
+        query = "update student set %s='%s' where sno='%s'" % (column, new, Sno)
+    elif column == 'sage':
+        new = int(new)
+        query = "update student set %s=%d where sno='%s'" % (column, new, Sno)
+    
+    try:
+        cursor.execute(query)
+    except mysql.connector.errors:
+        return False
+    cnx.commit()
+    return True
